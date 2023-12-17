e25586496aaa80605fb13ffcae44ad1a519f49cb
= <nsthien2109@gmail.com>
2023-12-17 23:42:36 +0700

    update vec

--- dist/api/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("../src/index");
-exports.default = index_1.default;
-//# sourceMappingURL=index.js.map
--- dist/src/shared/constants/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoleName = void 0;
-var RoleName;
-(function (RoleName) {
-    RoleName["ADMIN"] = "admin";
-    RoleName["USER"] = "user";
-})(RoleName = exports.RoleName || (exports.RoleName = {}));
-//# sourceMappingURL=index.js.map
